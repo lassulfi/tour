c95701d929098fe9839441cf3060f021e623e2d6
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-11-04 21:21:59 -0300

    Jackson DataBind

--- src/main/kotlin/com/github/lassulfi/tour/responses/CreatedResponse.kt
+++ src/main/kotlin/com/github/lassulfi/tour/responses/CreatedResponse.kt
@@ -1,5 +1,7 @@
 package com.github.lassulfi.tour.responses
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.util.*
 
-data class CreatedResponse (val message: String, val date: Date)
+data class CreatedResponse (val message: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") val date: Date)
